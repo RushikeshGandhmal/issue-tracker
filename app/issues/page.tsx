8cfe48e2d3ca3a1151c75a3a5a08462a43f351b0
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2023-11-30 23:05:11 +0530

    Build a custom styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,9 @@
 import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import React from "react";
 import IssueStatusBadge from "../component/IssueStatusBadge";
 import IssueActions from "./IssueActions";
+import Link from "../component/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -27,6 +27,9 @@ const IssuesPage = async () => {
           {issues?.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
+                {
+                  // NOTE : Below Link is Custom link, combining NextLink and RadixLink to add some style to identify links. It takes color from radix theme automatically if we change theme.
+                }
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
