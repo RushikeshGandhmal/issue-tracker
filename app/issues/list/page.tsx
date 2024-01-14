d2981857f99573dfd278dbe8e16ec6118409bb07
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-01-14 18:53:05 +0530

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -32,10 +32,18 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
