dbfc77f821e3aa69ea4a9beea3cf05c200e3cfa3
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-01-16 00:12:39 +0530

    change relationMode to prisma

--- prisma/migrations/20231126085541_add_default_status/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `issue` MODIFY `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN';
--- prisma/migrations/20231214061424_add_assigned_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `issue` ADD COLUMN `assignedToUserIs` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedToUserIs_fkey` FOREIGN KEY (`assignedToUserIs`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
